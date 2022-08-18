b15d14152eaded27733bb6101e43c23785b21fbd
lance <lance.li@yimlink.com>
2022-08-18 18:05:56 +0800

    refactor: feat(layout): 配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -1,24 +1,21 @@
 function createCZConfig() {
   return {
+    //可选类型
     types: [
-      { value: 'feat', name: '特性:    一个新的特性' },
-      { value: 'fix', name: '修复:    修复一个Bug' },
-      { value: 'docs', name: '文档:    变更的只有文档' },
-      {
-        value: 'style',
-        name: '格式:    空格, 分号等格式修复',
-      },
+      { value: 'feat', name: 'feat:   新功能' },
+      { value: 'fix', name: 'fix:   修复' },
+      { value: 'docs', name: 'docs:   文档变更' },
+      { value: 'style', name: 'style:   代码格式(不影响代码运行的变动)' },
       {
         value: 'refactor',
-        name: '重构:    代码重构，注意和特性、修复区分开',
+        name: 'refactor:重构(既不是增加feature)，也不是修复bug'
       },
-      { value: 'perf', name: '性能:    提升性能' },
-      { value: 'test', name: '测试:    添加一个测试' },
-      {
-        value: 'chore',
-        name: '工具:    开发工具变动(构建、脚手架工具等)',
-      },
-      { value: 'revert', name: '回滚:    代码回退' },
+      { value: 'perf', name: 'perf:   性能优化' },
+      { value: 'test', name: 'test:   增加测试' },
+      { value: 'chore', name: 'chore:   构建过程或辅助功能的变动' },
+      { value: 'revert', name: 'revert:   回退' },
+      { value: 'build', name: 'build:   打包' },
+      { value: 'revert', name: 'revert:   回退' }
     ],
 
     scopes: [{ name: 'component' }, { name: 'layout' }, { name: 'pages' }, { name: 'utils' }],
@@ -36,20 +33,17 @@ function createCZConfig() {
     */
     // override the messages, defaults are as follows
     messages: {
-      type: '选择您要提交的类型：',
-      scope: '选择一个scope (可选):',
-      // used if allowCustomScopes is true
-      customScope: 'Denote the SCOPE of this change:',
-      subject: '短说明:\n',
-      body: '长说明，使用"|"换行(可选)：\n',
-      breaking: '非兼容性说明 (可选):\n',
-      footer: '关联的issue，例如：#31, #34(可选):\n',
-      confirmCommit: '确定提交说明?',
+      type: '请选择提交类型',
+      customScope: '请输入修改范围(可选)',
+      subject: '请简要描述提交(必填)',
+      body: '请输入详细描述(可选)',
+      footer: '请输入要关闭的issue(可选)',
+      confirmCommit: '确认以上信息提交?(y/n)'
     },
 
     allowCustomScopes: true,
     allowBreakingChanges: ['feat', 'fix'],
-
+    skipQuestion: ['body', 'footer'],
     // limit subject length
     subjectLimit: 100,
   }
